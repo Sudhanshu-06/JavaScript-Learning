06643a3d059401eba484a9b93975e7cd01ed2e28
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-05-07 09:59:21 +0530

    push

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-console.log("Hello Sudhanshu");
-console.log(5+7);
-console.log(88+99);
-console.log(5+6-4*5)
+// var fileName='app2';
+// console.log("Hey there from app2",fileName);
+
+// variable- Named storage location, whose value can be manipulated during the execution of the program
+// window is a global object it is available on every javascript file
+
+// randomGuy="samson";
+// console.log(window.randomGuy)
+
+// var randomGuy="Sudhanshu"
+
+// let animal="Lion"
+// console.log(window.randomGuy,window.animal)
+// Data Types
+// - primitive-undefined,null,number,BigInt,Symbol,String,boolean
+// - Non-primitive - object
+
+function functionKaNaam(){
+    console.log("abbhhjvhvjhv")
+}
+functionKaNaam();
