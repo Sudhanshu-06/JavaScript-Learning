6e6568e77da666be799b1ff068748d644886a185
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-09-12 22:24:56 +0530

    done

---
+++ Javascript_Learn-coding/basic.js
@@ -0,0 +1,4 @@
+console.log("learn coding");
+// alert("Hare Krishna")
+// prompt("what is your name?");
+console.log("sudhanshu");
---
+++ Javascript_Learn-coding/basic1.js
@@ -0,0 +1,24 @@
+// Variables
+a=45;
+console.log(a);
+
+var 
+let 
+
+// var a; //declration 
+// a=45;  // intialization
+// console.log(a);
+
+var b=45;
+let courseId=123;
+const courseDept="computer Science"; 
+// console.log(courseId,courseDept);
+// console.table([courseDept,courseId,a]);
+
+courseId=245;
+// courseDept="Sciences"; 
+console.table([courseDept,courseId,a]);
+
+// const variable can't be re-assigned and  also not redeclared it
+// let can't re-declare but can re-assign it
+// var can be re-declared and also  redeclared it
