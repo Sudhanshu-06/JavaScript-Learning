9d21638dea5652e7745e78a707ac985b793c96ef
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-09-13 11:52:27 +0530

    done

---
+++ Javascript_Learn-coding/basic3.js
@@ -0,0 +1,45 @@
+// value to string
+
+let num1=45;
+console.log(typeof String(num1),num1);
+console.log(typeof num1.toString(),num1);
+
+let bool=false;
+console.log(typeof bool);
+console.log(typeof String(bool));
+
+
+let myName="Sudhanshu";
+console.log(myName);
+console.log(typeof myName);
+console.log(typeof Number(myName),myName);
+
+
+var bool1=false;
+console.log(typeof bool);
+console.log(typeof Number(bool));
+
+let myName1="Sudhanshu";
+console.log(myName);
+console.log(typeof myName);
+console.log(typeof Boolean(myName),myName);
+
+let num2=34;
+console.log(typeof Boolean(num2));
+
+
+// truthy and false
+// 0,-0,null,undefined
+
+// type coercion
+let name1=12+3+"Kishan";
+console.log(name1);
+
+let name2="Sudhanshu"+3+12;
+console.log(name2);
+
+let num4=12/'6';
+console.log(num4);
+
+
+
