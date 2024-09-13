561608d4b980f403be26a9c4ff9a48062de0a56b
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-09-13 23:24:12 +0530

    done

--- Javascript_Learn-coding/basic5.js
+++ Javascript_Learn-coding/basic5.js
@@ -1,33 +1,33 @@
-// for(let i=1; i<=5; i++){
-//     console.log(i);
-// }
-//while loop
-// let i=1
-// while(i<5){
-//     console.log(i);
-//     i++
-// }
+for(let i=1; i<=5; i++){
+    console.log(i);
+}
+// while loop
+let i=1
+while(i<5){
+    console.log(i);
+    i++
+}
 
 // do while loop
-// let j=1
-// do{
-//     console.log(i);
-//     i++
-// }while(i<=10)
+let j=1
+do{
+    console.log(i);
+    i++
+}while(i<=10)
 
 // for in loop work on object
 // for of loop work on array
 
-// for(let value of myArr){
-//     console.log(value);
-// }
+for(let value of myArr){
+    console.log(value);
+}
 
-// let myObj={
-//     key:"value"
-// }
-// for(let vla in obj){
-//     console.log(val);
-// }
+let myObj={
+    key:"value"
+}
+for(let vla in obj){
+    console.log(val);
+}
 
 for(let i=1; i<=100;i++){
     if(i%2===0){
